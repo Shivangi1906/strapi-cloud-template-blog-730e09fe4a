c026d6f3f7c49569aa78e1cbff532ce420717683
Shivangi1906 <shivangiravisingh06@gmail.com>
2025-11-15 19:00:34 +0530

    Added collections

---
+++ src/api/affiliate-country/controllers/affiliate-country.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * affiliate-country controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::affiliate-country.affiliate-country');
---
+++ src/api/affiliate-country/routes/affiliate-country.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * affiliate-country router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::affiliate-country.affiliate-country');
